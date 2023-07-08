3aaf26eba2ac4ee3f2dbde0559b63f8ac7b5a8b4
hakikicode <57021097+hakikicode@users.noreply.github.com>
2023-07-08 20:26:20 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
 import React from "react";
 import Routes from "./Routes";
 
+import { ThirdwebSDK } from "@thirdweb-dev/sdk/evm";
+
+const sdk = new ThirdwebSDK("fantom");
+const contract = await sdk.getContract("0xc8b19C1FE7c3C6a7F63588727d118049199aF10E");
+
 function App() {
   return <Routes />;
 }

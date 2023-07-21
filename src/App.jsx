323b255e70386ff7b7de5ffdcb873c1469910f68
hakikicode <57021097+hakikicode@users.noreply.github.com>
2023-07-21 16:51:53 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,15 @@ import Routes from "./Routes";
 
 import { ThirdwebSDK } from "@thirdweb-dev/sdk/evm";
 
+import { ThirdwebProvider, ConnectWallet, metamaskWallet } from "@thirdweb/react";
+import { ThirdwebProvider, ConnectWallet, walletConnect } from "@thirdweb/react";
 const sdk = new ThirdwebSDK("fantom");
 const contract = await sdk.getContract("0xc8b19C1FE7c3C6a7F63588727d118049199aF10E");
 
-function App() {
-  return <Routes />;
+export default function App() {
+return (
+    <ThirdwebProvider supportedWallets={[ metamaskWallet() ]}>
+      <ConnectWallet theme="light" />
+    </ThirdwebProvider>
+  );
 }
-
-export default App;
